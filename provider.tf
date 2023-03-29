7e9cd82e2f36a4d9535981b5e6df6fba938b1028
Mauro Pereira Filho <pereirafmauro@gmail.com>
2023-03-29 17:28:49 -0300

    feat: Adding provider config

---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.27.1"
+    }
+  }
+}
+
+provider "digitalocean" {
+  # Configuration options
+}
